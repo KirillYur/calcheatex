620e3df7fe8a3ccb4d042d849deac38f0f069253
Kirill Yurichkovskij <kl.yurichkovskij@gmail.com>
2024-09-17 22:36:08 +0300

    Calculation of coolant properties was done

--- calcheatex/plateheatex/calculations.py
+++ calcheatex/plateheatex/calculations.py
@@ -2,29 +2,50 @@ import math
 from iapws import IAPWS97
 import CoolProp.CoolProp as CP
 
-def coldDensity (tempColdIn, tempColdOut, pressureCold, selectTypeColdCoolant):
+def coldPropertiesCoolant (tempColdIn, tempColdOut, pressureCold, coldHumidity, selectTypeColdCoolant):
+    # Перевод едениц измерения
     tempColdIn = tempColdIn + 273.15
+    coldHumidity = coldHumidity / 100
     if selectTypeColdCoolant == "humidAir":
-        densityColdCoolant = (((pressureCold - CP.HAPropsSI('P_w','T',tempColdIn,'P',pressureCold,'R',0.1))/287.058)+(CP.HAPropsSI('P_w','T',tempColdIn,'P',pressureCold,'R',0.1)/461.522))/tempColdIn
-        return densityColdCoolant
-        # добавить выбор влажности и усреднение тем-р
+        densityColdCoolant = (((pressureCold - CP.HAPropsSI('P_w','T',tempColdIn,'P',pressureCold,'R',coldHumidity))/287.058)+(CP.HAPropsSI('P_w','T',tempColdIn,'P',pressureCold,'R',coldHumidity)/461.522))/tempColdIn
+        dynamicViscosityColdCoolant = CP.HAPropsSI("M", "T", tempColdIn,"P",pressureCold,'R',coldHumidity)
+        kinematicViscosityColdCoolant = (CP.HAPropsSI("M", "T", tempColdIn,"P",pressureCold,'R',coldHumidity))/densityColdCoolant
+        conductivityColdCoolant = CP.HAPropsSI("K", "T", tempColdIn,"P",pressureCold,'R',coldHumidity)
+        heatCapacityColdCoolant = CP.HAPropsSI("Cha", "T", tempColdIn,"P",pressureCold,'R',coldHumidity)
+        thermDiffusivityColdCoolant = conductivityColdCoolant / (densityColdCoolant*heatCapacityColdCoolant)
+        prandtlColdCoolant = kinematicViscosityColdCoolant / thermDiffusivityColdCoolant
+        # добавить усреднение температуры и проверку ошибки ввода тем-ры
     else:
         densityColdCoolant = CP.PropsSI("D", "T", tempColdIn, "P", pressureCold, selectTypeColdCoolant)
-        return densityColdCoolant
-    
-def hotDensity (tempHotIn, tempHotOut, pressureHot, selectTypeHotCoolant):
+        dynamicViscosityColdCoolant = CP.PropsSI("V", "T", tempColdIn,"P",pressureCold,selectTypeColdCoolant)
+        kinematicViscosityColdCoolant = (CP.PropsSI("V", "T", tempColdIn,"P",pressureCold,selectTypeColdCoolant))/densityColdCoolant
+        prandtlColdCoolant = CP.PropsSI("PRANDTL", "T", tempColdIn,"P",pressureCold,selectTypeColdCoolant)
+        conductivityColdCoolant = CP.PropsSI("L", "T", tempColdIn,"P",pressureCold,selectTypeColdCoolant)
+        heatCapacityColdCoolant = CP.PropsSI("C", "T", tempColdIn,"P",pressureCold,selectTypeColdCoolant)
+        thermDiffusivityColdCoolant = conductivityColdCoolant / (densityColdCoolant*heatCapacityColdCoolant)
+    return densityColdCoolant, dynamicViscosityColdCoolant, kinematicViscosityColdCoolant, prandtlColdCoolant, conductivityColdCoolant, heatCapacityColdCoolant, thermDiffusivityColdCoolant
+def hotPropertiesCoolant (tempHotIn, tempHotOut, pressureHot, hotHumidity, selectTypeHotCoolant):
+    # Перевод едениц измерения
+    hotHumidity = hotHumidity / 100
     tempHotIn = tempHotIn + 273.15
     if selectTypeHotCoolant == "humidAir":
-        densityHotCoolant = (((pressureHot - CP.HAPropsSI('P_w','T',tempHotIn,'P',pressureHot,'R',0.1))/287.058)+(CP.HAPropsSI('P_w','T',tempHotIn,'P',pressureHot,'R',0.1)/461.522))/tempHotIn
-         # добавить выбор влажности и усреднение тем-р
-        return densityHotCoolant
+        densityHotCoolant = (((pressureHot - CP.HAPropsSI('P_w','T',tempHotIn,'P',pressureHot,'R',hotHumidity))/287.058)+(CP.HAPropsSI('P_w','T',tempHotIn,'P',pressureHot,'R',hotHumidity)/461.522))/tempHotIn
+        dynamicViscosityHotCoolant = CP.HAPropsSI("M", "T", tempHotIn,"P",pressureHot,'R',hotHumidity)
+        kinematicViscosityHotCoolant = (CP.HAPropsSI("M", "T", tempHotIn,"P",pressureHot,'R',hotHumidity))/densityHotCoolant
+        conductivityHotCoolant = CP.HAPropsSI("K", "T", tempHotIn,"P",pressureHot,'R',hotHumidity)
+        heatCapacityHotCoolant = CP.HAPropsSI("Cha", "T", tempHotIn,"P",pressureHot,'R',hotHumidity)
+        thermDiffusivityHotCoolant = conductivityHotCoolant / (densityHotCoolant*heatCapacityHotCoolant)
+        prandtlHotCoolant = kinematicViscosityHotCoolant / thermDiffusivityHotCoolant
+         # добавить усреднение температуры и проверку ошибки ввода тем-ры
     else:
-        densityHotCoolant = CP.PropsSI("D", "T", tempHotIn, "P", pressureHot, selectTypeHotCoolant)
-        return densityHotCoolant
-
-# def calcThermDiffusivity(thermConductivity, substanceDensity, heatCapacity):
-#     thermDiffusivity = thermConductivity/(substanceDensity*heatCapacity*10**3)
-#     return thermDiffusivity
+        densityHotCoolant = CP.PropsSI("D","T", tempHotIn,"P",pressureHot,selectTypeHotCoolant)
+        dynamicViscosityHotCoolant = CP.PropsSI("V", "T", tempHotIn,"P",pressureHot,selectTypeHotCoolant)
+        kinematicViscosityHotCoolant = (CP.PropsSI("V", "T", tempHotIn,"P",pressureHot,selectTypeHotCoolant))/densityHotCoolant
+        prandtlHotCoolant = CP.PropsSI("PRANDTL", "T", tempHotIn,"P",pressureHot,selectTypeHotCoolant)
+        conductivityHotCoolant = CP.PropsSI("L", "T", tempHotIn,"P",pressureHot,selectTypeHotCoolant)
+        heatCapacityHotCoolant = CP.PropsSI("C", "T", tempHotIn,"P",pressureHot,selectTypeHotCoolant)
+        thermDiffusivityHotCoolant = conductivityHotCoolant / (densityHotCoolant*heatCapacityHotCoolant)
+    return densityHotCoolant, dynamicViscosityHotCoolant, kinematicViscosityHotCoolant, prandtlHotCoolant, conductivityHotCoolant, heatCapacityHotCoolant, thermDiffusivityHotCoolant
 
 # def calcPartialPressure(inputTemperature):
 #     partialPressure = 133.3*math.e**(18.6-(3992/(inputTemperature+233.8)))
--- calcheatex/plateheatex/views.py
+++ calcheatex/plateheatex/views.py
@@ -11,26 +11,62 @@ def calc(request):
 
         tempHotIn = float(request.POST.get('tempHotIn', 0))
         tempHotOut = float(request.POST.get('tempHotOut', 0))
-        flowHot = float(request.POST.get('flowHot', 0))
         pressureHot = float(request.POST.get('pressureHot', 0))
+        selectTypeHotCoolant = str(request.POST.get('selectTypeHotCoolant'))
 
         tempColdIn = float(request.POST.get('tempColdIn', 0))
         tempColdOut = float(request.POST.get('tempColdOut', 0))
-        flowCold = float(request.POST.get('flowCold', 0))
         pressureCold = float(request.POST.get('pressureCold', 0))
-        
         selectTypeColdCoolant = str(request.POST.get('selectTypeColdCoolant'))
-        selectTypeHotCoolant = str(request.POST.get('selectTypeHotCoolant'))
+        
+        flowHot = request.POST.get('flowHot', 0)
+        if flowHot == '':
+                flowHot = 0
+        else:
+                flowHot = float(flowHot)
+        
+        hotHumidity = request.POST.get('hotHumidity', 0)
+        if hotHumidity == '':
+                hotHumidity = 0
+        else:
+                hotHumidity = float(hotHumidity)
 
-        resultHotDensity = hotDensity(tempHotIn, tempHotOut, pressureHot, selectTypeHotCoolant)
-        resultColdDensity = coldDensity(tempColdIn, tempColdOut, pressureCold, selectTypeColdCoolant)
+        flowCold = request.POST.get('flowCold', 0)
+        if flowCold == '':
+                flowCold = 0
+        else:
+                flowCold = float(flowCold)
+        
+        coldHumidity = request.POST.get('coldHumidity', 0)
+        if coldHumidity == '':
+                coldHumidity = 0
+        else:
+                coldHumidity = float(coldHumidity)
 
+        densityColdCoolant, dynamicViscosityColdCoolant, kinematicViscosityColdCoolant, prandtlColdCoolant, conductivityColdCoolant, heatCapacityColdCoolant, thermDiffusivityColdCoolant = hotPropertiesCoolant(tempHotIn, tempHotOut, pressureHot, hotHumidity, selectTypeHotCoolant)
+        densityHotCoolant, dynamicViscosityHotCoolant, kinematicViscosityHotCoolant, prandtlHotCoolant, conductivityHotCoolant, heatCapacityHotCoolant, thermDiffusivityHotCoolant = coldPropertiesCoolant(tempColdIn, tempColdOut, pressureCold, coldHumidity, selectTypeColdCoolant)
+        
+        propertiesCoolants = {
+        'densityColdCoolant': round(densityColdCoolant, 3),
+        'dynamicViscosityColdCoolant': f"{dynamicViscosityColdCoolant:.3e}",
+        'kinematicViscosityColdCoolant': f"{kinematicViscosityColdCoolant:.3e}",
+        'prandtlColdCoolant': round(prandtlColdCoolant, 3),
+        'conductivityColdCoolant': round(conductivityColdCoolant, 3),
+        'heatCapacityColdCoolant': round(heatCapacityColdCoolant, 3),
+        'thermDiffusivityColdCoolant': f"{thermDiffusivityColdCoolant:.3e}",
+
+        'densityHotCoolant': round(densityHotCoolant, 3),
+        'dynamicViscosityHotCoolant': f"{dynamicViscosityHotCoolant:.3e}",
+        'kinematicViscosityHotCoolant': f"{kinematicViscosityHotCoolant:.3e}",
+        'prandtlHotCoolant': round(prandtlHotCoolant, 3),
+        'conductivityHotCoolant': round(conductivityHotCoolant, 3),
+        'heatCapacityHotCoolant': round(heatCapacityHotCoolant, 3),
+        'thermDiffusivityHotCoolant': f"{thermDiffusivityHotCoolant:.3e}",
+        }
         # resultPartialPressure = calcPartialPressure(inputTemperature)
 
-        return JsonResponse({'resultHotDensity' : resultHotDensity,
-                             'resultColdDensity' : resultColdDensity,
-                        #       'resultPartialPressure' : resultPartialPressure
-                        })
+        return JsonResponse({'propertiesCoolants' : propertiesCoolants
+        })
 
 def types(request, types_id):
         return HttpResponse ("Теплообменники по типам")
